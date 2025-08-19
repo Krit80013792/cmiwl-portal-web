7d45139e5f0f2b48250c6aedfd249ecc844954cf
Woraphon Khuarprakopkit <woraphon.k@tidlor.com>
2025-08-19 18:58:43 +0700

    ✨ feat: dev

--- src/shared/utils/redis.ts
+++ src/shared/utils/redis.ts
@@ -3,6 +3,8 @@ import Redis from "ioredis";
 const redis = new Redis({
     host: process.env.REDIS_ENDPOINT || "localhost",
     port: Number(process.env.REDIS_PORT) || 6379,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
     db: Number(process.env.REDIS_DEFAULTDATABASE) || 0,
     connectTimeout: (Number(process.env.REDIS_TIMEOUTMINUTES) || 5),
     keyPrefix: process.env.REDIS_CHANNELPREFIX ? `${process.env.REDIS_CHANNELPREFIX}:` : undefined,
