9059df747d7e4af2174a6fa9563482d111a09721
Apisek Boonsawathirun <apisek.b@tidlor.com>
2025-10-30 14:24:57 +0700

    fix: update middleware to handle '/th/PaymentCredit' route instead of '/th/waiting'

--- middleware.ts
+++ middleware.ts
@@ -80,7 +80,7 @@ export default async function middleware(req: NextRequest) {
   const resNext = NextResponse.next()
   const path = req.nextUrl.pathname
 
-  if (path === '/th/waiting') {
+  if (path === '/th/PaymentCredit') {
     return resNext
   }
 
