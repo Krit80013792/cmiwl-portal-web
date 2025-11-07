f20758a480c39cbfe5499f16076abadf85b859ef
Apisek Boonsawathirun <apisek.b@tidlor.com>
2025-11-07 09:27:54 +0700

    fix: add Cross-Origin policies to enhance security in next.config.js

--- next.config.js
+++ next.config.js
@@ -39,6 +39,10 @@ const nextConfig = {
               "frame-ancestors 'none'",
             ].join('; '),
           },
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+          { key: 'Cross-Origin-Resource-Policy', value: 'same-origin' },
+
           {
             key: 'X-Frame-Options',
             value: 'DENY',
