c372928a87426a60353cf74deba646131a73b6c5
Krit Ic <80013792@tidlor.com>
2026-03-09 11:42:17 +0700

    cetagory list and textstyling

--- app/(main)/th/VehicleCTP/_components/VehicleType/VehicleType.tsx
+++ app/(main)/th/VehicleCTP/_components/VehicleType/VehicleType.tsx
@@ -91,12 +91,11 @@ const VehicleCategory = ({ channel }: { channel: any }) => {
             <div className="col-6 pe-2" key={e.itemOrder}>
               <button onClick={() => handleVehicleCategory(e, idx)} type="button" className="w-100 h-100">
                 <div className={`py-12 px-3 rounded-4 choice-card text-center h-100${isActive ? ' active' : ''}`}>
-                  <Image src={`data:image/png;base64,${e.imagePath}`} alt={e.displayName} width={80} height={42} />
-                  <p className="mb-0 text-center text-grey">
+                  <img className='vehicle-category-image' src={`/assets/vehicle-category/${idx}.svg`} alt={e.displayName} width={123} height={56} />
+                  <p className="vehicle-category-image mb-0 text-center text-grey">
                     {e.displayName.split(':').map((line: string) => (
                       <span key={line}>
                         {line}
-                        <br />
                       </span>
                     ))}
                   </p>
